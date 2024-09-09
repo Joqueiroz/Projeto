d1460cdca867033575a54eaae80616de8f5948b0
Joqueiroz <joaoqueiroz964@gmail.com>
2024-09-09 10:43:54 -0300

    Sprint 1

---
+++ Back-end_Sprint1/src/server.js
@@ -0,0 +1,4 @@
+//Importar a instância do Express configurada em index.js
+const app = require("./index");
+// neste caso a API será acessível em http://localhost:3000/Agendamento-sala-senai/health/
+app.listen(3000);
